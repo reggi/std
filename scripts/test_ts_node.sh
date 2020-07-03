ac842fb9aab26cc59f5bb4f9621836f053ea95c6
reggi <thomas@reggi.com>
2020-07-03 05:14:03 -0400

    fix

--- scripts/test_ts_node.sh
+++ scripts/test_ts_node.sh
@@ -5,5 +5,5 @@ BASEDIR="$(cd "$(dirname "$0")"; pwd)"
 . $BASEDIR/npm_bin.sh
 
 test_ts_node () {
-  npm_bin mocha -r ts-node/register "./ts-node/module/*_test.ts"
+  npm_bin mocha -r ts-node/register `./ts-node/module/*_test.ts`
 }
