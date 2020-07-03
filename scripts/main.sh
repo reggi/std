315156cee697e5ae67b8b55bfb7bf93bd1136b19
reggi <thomas@reggi.com>
2020-07-03 04:55:55 -0400

    fix

--- scripts/main.sh
+++ scripts/main.sh
@@ -6,30 +6,34 @@ BASEDIR=$(dirname "$0")
 
 # define variables
 FILES="$(find "$BASEDIR" ! -name 'main.sh' -type f -name '*.sh' -print | sort -t '\0' -n)"
-TUSAGE="\\n"
-USAGE=""
-DESC=""
-FOUND=false
 
-# loop over script files
-for FILE in $FILES; do
-  if [ -f "$FILE" ]; then
-    # shellcheck source=/dev/null
-    . "$FILE" --source-only
-    FNAME=$(file_name "$FILE")
-    BUSAGE="$FNAME $USAGE"
-    TUSAGE="$TUSAGE\\t$BUSAGE\\n              $DESC\n\n"
-    USAGE=""
-    if [ "$1" = "$FNAME" ]; then
-      FOUND=true
-    fi
-  fi
-done
+echo "$FILES"
 
-if [ "$FOUND" = true ]; then
-  "$@"
-else
-  printf "Usage: <...args>\n"
-  printf "$TUSAGE"
-  exit 1
-fi
+# TUSAGE="\\n"
+# USAGE=""
+# DESC=""
+# FOUND=false
+
+# # loop over script files
+# for FILE in $FILES; do
+#   if [ -f "$FILE" ]; then
+#     # shellcheck source=/dev/null
+#     . "$FILE" --source-only
+#     FNAME=$(file_name "$FILE")
+#     BUSAGE="$FNAME $USAGE"
+#     TUSAGE="$TUSAGE\\t$BUSAGE\\n              $DESC\n\n"
+#     USAGE=""
+#     if [ "$1" = "$FNAME" ]; then
+#       FOUND=true
+#     fi
+#   fi
+# done
+
+# if [ "$FOUND" = true ]; then
+#   "$@"
+# else
+#   printf "Usage: <...args>\n"
+#   # shellcheck disable=SC2059
+#   printf "$TUSAGE"
+#   exit 1
+# fi
